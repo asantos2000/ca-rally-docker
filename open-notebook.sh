c527f01c1d9944f3e37386ed08f3dc2d7aada7ac
Anderson Santos <adsantos@gmail.com>
2018-08-11 12:06:04 -0300

    Add support to Linux (tested on Ubuntu)

--- open-notebook.sh
+++ open-notebook.sh
@@ -1,1 +1,13 @@
-open `docker exec -it prally jupyter notebook list | sed -n 2p | awk '{print $1;}'`
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux
+                xdg-open `docker exec -it prally jupyter notebook list | sed -n 2p | awk '{print $1;}'`
+    ;;
+    Darwin*)    machine=Mac
+                open `docker exec -it prally jupyter notebook list | sed -n 2p | awk '{print $1;}'`
+    ;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo "Running on " ${machine}
