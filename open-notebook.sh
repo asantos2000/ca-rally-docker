f3528d502558a5d926a6487813f435c305234581
Anderson Santos <adsantos@gmail.com>
2018-08-12 15:35:46 -0300

    Add support MINGW

--- open-notebook.sh
+++ open-notebook.sh
@@ -1,13 +1,15 @@
+URL=`docker exec -it prally jupyter notebook list | sed -n 2p | awk '{print $1;}'`
+
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)     machine=Linux
-                xdg-open `docker exec -it prally jupyter notebook list | sed -n 2p | awk '{print $1;}'`
-    ;;
+                xdg-open $URL;;
     Darwin*)    machine=Mac
-                open `docker exec -it prally jupyter notebook list | sed -n 2p | awk '{print $1;}'`
-    ;;
-    CYGWIN*)    machine=Cygwin;;
-    MINGW*)     machine=MinGw;;
+                open $URL;;
+    CYGWIN*)    machine=Cygwin
+                xdg-open $URL;;
+    MINGW*)     machine=MinGw
+                start $URL;;
     *)          machine="UNKNOWN:${unameOut}"
 esac
 echo "Running on " ${machine}
